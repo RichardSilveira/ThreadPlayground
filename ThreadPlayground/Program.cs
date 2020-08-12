4153dc70bb118d8361457476139b2e7a642c516f
Richard Silveira <richardleecba@gmail.com>
2020-08-12 06:41:10 -0400

    =]

--- ThreadPlayground/Program.cs
+++ ThreadPlayground/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using static System.Console;
 
@@ -8,7 +10,7 @@ namespace ThreadPlayground
     {
         static void Main(string[] args)
         {
-            //RandomTests.Start(args);
+            RandomTests.Start(args);
             //ThreadBasics.Start(args);
 
             //ExclusiveLock.Start();
@@ -21,6 +23,53 @@ namespace ThreadPlayground
             //ParallelForEach.Start();
             //DeadLock.Start();
 
+            /*var data = new List<string>()
+            {
+                "richard",
+                "richard",
+                "richard",
+                "julieth",
+                "julieth",
+                "floripa"
+            };
+
+            data = new List<string>();
+
+            var result = freqCnt(data);*/
+        }
+
+
+        public static Dictionary<T, Int32> freqCnt<T>(IEnumerable<T> data)
+        {
+            return data.ToList().Aggregate(new Dictionary<T, Int32>(), (acc, item) =>
+            {
+                if (acc.ContainsKey(item))
+                {
+                    Int32 value = 0;
+                    acc.TryGetValue(item, out value);
+                    acc[item] = value + 1;
+                }
+                else
+                    acc.Add(item, 1);
+
+                return acc;
+            });
+/*
+
+            for (Int32 i = 0; i < data.Count(); i++)
+            {
+                if (result.ContainsKey(data[i]))
+                {
+                    Int32 value = 0;
+                    result.TryGetValue(data[i], out value);
+                    result.Remove(data[i]); // -- otherwise an exception on the next line?!?
+                    result.Add(data[i], value + 1);
+                }
+                else
+                    result.Add(data[i], 1);
+            }
+
+            return result;*/
         }
     }
 }
--- ThreadPlayground/RandomTests.cs
+++ ThreadPlayground/RandomTests.cs
@@ -14,21 +14,26 @@ namespace ThreadPlayground
 
             WriteLine("intensive work on Main Thread after blocked thread");
             //intensive work
-            for (int i = 0; i < 10_000_000; i++) { }
+            for (int i = 0; i < 10_000_000; i++)
+            {
+            }
 
             WriteLine("END");
         }
 
         static async Task<int> InvokeOperationAsync()
         {
-            var opTask = ChildOperationDelay0();
+            //var opTask = Task.Run(ChildOperationDelay0);
+            var opTask = Task.Run(ChildOperationSync2);
 
             WriteLine("START intensive work on InvokeOperationAsync");
 
             //intensive work
-            for (int i = 0; i < 80_000_000; i++) { }
+            for (int i = 0; i < 40_000_000; i++)
+            {
+            }
 
-            WriteLine("END intensive work on InvokeOperationAsync (waiting for async child operation is done)");
+            WriteLine("END intensive work on InvokeOperationAsync");
 
             var result = await opTask;
 
@@ -40,7 +45,9 @@ namespace ThreadPlayground
         {
             WriteLine("intensive work on ChildOperationSync");
             //intensive work
-            for (int i = 0; i < 100_000_000; i++) { }
+            for (int i = 0; i < 100_000_000; i++)
+            {
+            }
 
             return Task.FromResult<int>(100);
         }
@@ -51,7 +58,9 @@ namespace ThreadPlayground
             {
                 WriteLine("intensive work on Anonymous func on ChildOpTaskRun");
                 //intensive work
-                for (int i = 0; i < 100_000_000; i++) { }
+                for (int i = 0; i < 100_000_000; i++)
+                {
+                }
 
                 return 100;
             });
@@ -61,9 +70,27 @@ namespace ThreadPlayground
         {
             await Task.Delay(0);
 
-            WriteLine("intensive work on ChildOperationDelay0");
+            WriteLine("START intensive work on ChildOperationDelay0");
             //intensive work
-            for (int i = 0; i < 100_000_000; i++) { }
+            for (int i = 0; i < 100_000_000; i++)
+            {
+            }
+
+            WriteLine("END intensive work on ChildOperationDelay0");
+
+
+            return 100;
+        }
+
+        static int ChildOperationSync2()
+        {
+            WriteLine("START intensive work on ChildOperationSync");
+            //intensive work
+            for (int i = 0; i < 100_000_000; i++)
+            {
+            }
+
+            WriteLine("END intensive work on ChildOperationSync");
 
             return 100;
         }
@@ -71,7 +98,9 @@ namespace ThreadPlayground
         static Task Teste()
         {
             //intensive work
-            for (int i = 0; i < 100_000_000; i++) { }
+            for (int i = 0; i < 100_000_000; i++)
+            {
+            }
 
             return Task.CompletedTask;
         }
